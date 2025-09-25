bd11a88e34b76a0ab4abb6c5fd550fac314ec4d7
changhui98 <143060426+changhui98@users.noreply.github.com>
2025-09-25 09:36:18 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int num) {
+    
+        
+        return isEvenOdd(num);
+    }
+    
+    private String isEvenOdd(int num){
+        String isEvenOdd = "";
+        
+        return isEvenOdd = (num % 2 == 0) ? "Even" : "Odd";
+    }
+}
