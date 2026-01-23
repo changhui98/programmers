fdb2b78ff9b0965b4efd064bcd4260d4f5b6737d
changhui98 <yuch0102@gmail.com>
2026-01-23 16:33:31 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        
+        if(n <= 7){
+            return 1;
+        }
+        
+        if(n % 7 != 0){
+            return (n / 7) + 1;
+        } else {
+            return (n / 7);
+        }
+    }
+}
